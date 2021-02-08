10dbfaf0b3f60e169a26b7cc6685463e72ae5ed2
Super262 <virusformail@gmail.com>
2021-02-08 21:57:52 +0800

    Finished Problem 680.

---
+++ doublepointers/problem0680.go
@@ -0,0 +1,34 @@
+package doublepointers
+
+func isPalindrome(charArray []rune, start int, end int) bool {
+	for start <= end {
+		if charArray[start] != charArray[end] {
+			return false
+		}
+		start++
+		end--
+	}
+	return true
+}
+
+func validPalindrome(s string) bool {
+	charArray := []rune(s)
+	i := 0
+	j := len(charArray) - 1
+	for i < j {
+		if charArray[i] != charArray[j] {
+			break
+		}
+		i++
+		j--
+	}
+	if i >= j {
+		return true
+	} else {
+		if isPalindrome(charArray, i, j-1) || isPalindrome(charArray, i+1, j) {
+			return true
+		} else {
+			return false
+		}
+	}
+}
