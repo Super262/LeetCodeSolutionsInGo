0dc750677075879e7362403a1d0525ab96d8e23b
Super262 <virusformail@gmail.com>
2021-02-11 09:20:08 +0800

    Finished Problem 695.

--- searchmethods/dfs/problem0695.go
+++ searchmethods/dfs/problem0695.go
@@ -1,19 +1,19 @@
 package dfs
 
-func dfsGetLength(grid *[][]int, startY int, startX int, direction *[][]int) int {
+func dfsGetArea0695(grid *[][]int, startY int, startX int, direction *[][]int) int {
 	if startX < 0 || startY < 0 || startX >= len((*grid)[0]) || startY >= len(*grid) || (*grid)[startY][startX] == 0 {
 		return 0
 	}
-	result := 1
+	area := 1
 	nextY := 0
 	nextX := 0
 	(*grid)[startY][startX] = 0
 	for _, d := range *direction {
 		nextY = d[0] + startY
 		nextX = d[1] + startX
-		result += dfsGetLength(grid, nextY, nextX, direction)
+		area += dfsGetArea0695(grid, nextY, nextX, direction)
 	}
-	return result
+	return area
 }
 
 func maxAreaOfIsland(grid [][]int) int {
@@ -34,7 +34,7 @@ func maxAreaOfIsland(grid [][]int) int {
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
 			if grid[y][x] == 1 {
-				tempResult = dfsGetLength(&grid, y, x, &direction)
+				tempResult = dfsGetArea0695(&grid, y, x, &direction)
 				if tempResult > result {
 					result = tempResult
 				}
