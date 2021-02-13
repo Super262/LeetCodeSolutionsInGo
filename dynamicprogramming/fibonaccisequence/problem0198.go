bea15109d584158e4b6e17b1eed5fed0c6423d40
Super262 <virusformail@gmail.com>
2021-02-13 23:44:33 +0800

    Finished Problem 198.

---
+++ dynamicprogramming/fibonaccisequence/problem0198.go
@@ -0,0 +1,26 @@
+package fibonaccisequence
+
+func rob(nums []int) int {
+	if nums == nil {
+		return 0
+	}
+	numsLen := len(nums)
+	if numsLen == 0 {
+		return 0
+	}
+	f0 := 0
+	f1 := nums[0]
+	fn := f1 + f0
+	tempSum := 0
+	for l := 2; l <= numsLen; l++ {
+		tempSum = nums[l-1] + f0
+		if tempSum > f1 {
+			fn = tempSum
+		} else {
+			fn = f1
+		}
+		f0 = f1
+		f1 = fn
+	}
+	return fn
+}
