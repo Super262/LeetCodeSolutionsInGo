a33efb340f8455af112ddba23e944192fced716a
Super262 <virusformail@gmail.com>
2021-02-15 15:28:40 +0800

    Finished Problem 583.

---
+++ dynamicprogramming/editstring/problem0583.go
@@ -0,0 +1,36 @@
+package editstring
+
+func getMinInt0583(a int, b int) int {
+	if a > b {
+		return b
+	} else {
+		return a
+	}
+}
+
+func minDistance(word1 string, word2 string) int {
+	word1Array := []rune(word1)
+	word2Array := []rune(word2)
+	word1Len := len(word1Array)
+	word2Len := len(word2Array)
+	dp := make([][]int, word1Len+1, word1Len+1)
+	for i := range dp {
+		dp[i] = make([]int, word2Len+1, word2Len+1)
+	}
+	for l := 1; l <= word1Len; l++ {
+		dp[l][0] = l
+	}
+	for l := 1; l <= word2Len; l++ {
+		dp[0][l] = l
+	}
+	for l1 := 1; l1 <= word1Len; l1++ {
+		for l2 := 1; l2 <= word2Len; l2++ {
+			if word1Array[l1-1] == word2Array[l2-1] {
+				dp[l1][l2] = dp[l1-1][l2-1]
+			} else {
+				dp[l1][l2] = getMinInt0583(dp[l1-1][l2]+1, dp[l1][l2-1]+1)
+			}
+		}
+	}
+	return dp[word1Len][word2Len]
+}
