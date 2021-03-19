9594b12c202796dfcb2c567d779350d9d0a02a92
Super262 <virusformail@gmail.com>
2021-03-19 14:37:35 +0800

    Finished Problem 55.

--- algorithm/greedystrategy/problem0055.go
+++ algorithm/greedystrategy/problem0055.go
@@ -1,30 +1,22 @@
 package greedystrategy
 
 func canJump(nums []int) bool {
-	if nums == nil {
+	if nums == nil || len(nums) == 0 {
 		return false
 	}
-	currentLimit := 0
-	start := 0
-	nextStart := 0
-	for start < len(nums) {
-		currentLimit = start + nums[start]
-
-		// 不能改变下面的两个if语句块的顺序，应对特殊情况：nums = [1]
-		if currentLimit >= len(nums)-1 {
+	globalBound := 0
+	currentBound := 0
+	for i := 0; i < len(nums); i++ {
+		if i > globalBound {
+			continue
+		}
+		currentBound = i + nums[i]
+		if currentBound > globalBound {
+			globalBound = currentBound
+		}
+		if globalBound >= len(nums)-1 {
 			return true
 		}
-		if currentLimit <= start {
-			return false
-		}
-
-		nextStart = start + 1
-		for i := start + 2; i <= currentLimit; i++ {
-			if i+nums[i] >= nextStart+nums[nextStart] {
-				nextStart = i
-			}
-		}
-		start = nextStart
 	}
 	return false
 }
