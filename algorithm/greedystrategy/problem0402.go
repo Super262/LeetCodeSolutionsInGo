fe8e9be51fd0ea0172976661e37becd7ade73e9d
Super262 <virusformail@gmail.com>
2021-03-20 09:38:11 +0800

    Finished Problem 402.

--- algorithm/greedystrategy/problem0402.go
+++ algorithm/greedystrategy/problem0402.go
@@ -4,16 +4,16 @@ func removeKdigits(num string, k int) string {
 	if num == "" || k >= len(num) {
 		return "0"
 	}
-	deletedItemsCount := k
+	deletedItemsCount := 0
 	numArray := []rune(num)
 	stack := make([]rune, len(numArray), len(numArray))
 	stackTop := -1
 	digit := '\u0000'
 	for i := 0; i < len(numArray); i++ {
 		digit = numArray[i]
-		for stackTop >= 0 && deletedItemsCount > 0 && stack[stackTop] > numArray[i] {
+		for stackTop >= 0 && deletedItemsCount < k && stack[stackTop] > numArray[i] {
 			stackTop--
-			deletedItemsCount--
+			deletedItemsCount++
 		}
 		if numArray[i] == '0' && stackTop == -1 {
 			continue
@@ -21,12 +21,12 @@ func removeKdigits(num string, k int) string {
 		stackTop++
 		stack[stackTop] = digit
 	}
+	for deletedItemsCount < k {
+		deletedItemsCount++
+		stackTop--
+	}
 	if stackTop == -1 {
 		return "0"
 	}
-	if len(numArray)-k <= stackTop+1 {
-		return string(stack[0 : len(numArray)-k])
-	} else {
-		return string(stack[0 : stackTop+1])
-	}
+	return string(stack[0 : stackTop+1])
 }
