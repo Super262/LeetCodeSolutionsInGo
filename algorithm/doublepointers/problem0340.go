c763a31b0a94ffdd719e11f7e3aae30c11ee40ae
Super262 <virusformail@gmail.com>
2021-03-22 10:44:54 +0800

    Finished Problem 340.

---
+++ algorithm/doublepointers/problem0340.go
@@ -0,0 +1,31 @@
+package doublepointers
+
+func lengthOfLongestSubstringKDistinct(s string, k int) int {
+	if s == "" || k == 0 {
+		return 0
+	}
+	sArray := []rune(s)
+	chFreq := make([]int, 256, 256)
+	result := 0
+	uniqueCount := 0
+	left := 0
+	right := 0
+	for right < len(sArray) {
+		chFreq[int(sArray[right])]++
+		if chFreq[int(sArray[right])] == 1 {
+			uniqueCount++
+		}
+		right++
+		for left < right && uniqueCount > k {
+			chFreq[int(sArray[left])]--
+			if chFreq[int(sArray[left])] == 0 {
+				uniqueCount--
+			}
+			left++
+		}
+		if right-left > result {
+			result = right - left
+		}
+	}
+	return result
+}
