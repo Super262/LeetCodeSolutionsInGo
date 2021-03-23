d7a9a9623aac7b18003ff0cdc83e7c2bbe958ad7
Super262 <virusformail@gmail.com>
2021-03-23 21:22:05 +0800

    Finished Problem 283.

---
+++ algorithm/doublepointers/problem0283.go
@@ -0,0 +1,20 @@
+package doublepointers
+
+func moveZeroes(nums []int) {
+	if nums == nil || len(nums) == 0 {
+		return
+	}
+	left := 0
+	right := 0
+	for right < len(nums) {
+		if nums[right] != 0 {
+			nums[left] = nums[right]
+			left++
+		}
+		right++
+	}
+	for left < len(nums) {
+		nums[left] = 0
+		left++
+	}
+}
