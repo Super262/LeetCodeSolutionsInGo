75586f20e1c3849f5b3f3a925164f8b06aab5344
Super262 <virusformail@gmail.com>
2021-03-25 22:05:57 +0800

    Finished Problem 173.

--- datastructure/tree/problem0173.go
+++ datastructure/tree/problem0173.go
@@ -6,24 +6,24 @@ type BSTIterator struct {
 
 func Constructor(root *TreeNode) BSTIterator {
 	iter := BSTIterator{make([]*TreeNode, 0, 0)}
-	for root != nil {
-		iter.stack = append(iter.stack, root)
-		root = root.Left
-	}
+	iter.FindMostLeft(root)
 	return iter
 }
 
 func (this *BSTIterator) Next() int {
 	curNode := this.stack[len(this.stack)-1]
 	this.stack = this.stack[0 : len(this.stack)-1]
-	nextNode := curNode.Right
-	for nextNode != nil {
-		this.stack = append(this.stack, nextNode)
-		nextNode = nextNode.Left
-	}
+	this.FindMostLeft(curNode.Right)
 	return curNode.Val
 }
 
 func (this *BSTIterator) HasNext() bool {
 	return len(this.stack) > 0
+}
+
+func (this *BSTIterator) FindMostLeft(root *TreeNode) {
+	for root != nil {
+		this.stack = append(this.stack, root)
+		root = root.Left
+	}
 }
