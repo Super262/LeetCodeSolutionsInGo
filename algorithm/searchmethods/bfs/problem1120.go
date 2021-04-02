d0e7c1f365f1c0eeb9fcca97b38ec073c55665a5
Super262 <virusformail@gmail.com>
2021-04-02 19:10:31 +0800

    Finished Problem 1120.

---
+++ algorithm/searchmethods/bfs/problem1120.go
@@ -0,0 +1,21 @@
+package bfs
+
+import "math"
+
+func maximumAverageSubtree(root *TreeNode) float64 {
+	_, _, result := helper1120(root)
+	return result
+}
+
+func helper1120(root *TreeNode) (int, int, float64) {
+	if root == nil {
+		return 0, 0, -1
+	}
+	leftNodesCount, leftValuesSum, leftAvgMax := helper1120(root.Left)
+	rightNodesCount, rightValuesSum, rightAvgMax := helper1120(root.Right)
+	rootNodesCount := 1 + leftNodesCount + rightNodesCount
+	rootValuesSum := root.Val + leftValuesSum + rightValuesSum
+	rootAvg := float64(rootValuesSum) / float64(rootNodesCount)
+	rootAvgMax := math.Max(rootAvg, math.Max(leftAvgMax, rightAvgMax))
+	return rootNodesCount, rootValuesSum, rootAvgMax
+}
