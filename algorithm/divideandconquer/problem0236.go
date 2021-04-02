72e30429e7d34b46b6680181933988f4c41bd8a7
Super262 <virusformail@gmail.com>
2021-04-02 20:07:45 +0800

    Finished Problem 236.

---
+++ algorithm/divideandconquer/problem0236.go
@@ -0,0 +1,19 @@
+package divideandconquer
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || q == nil || p == nil {
+		return nil
+	}
+	if root == p || q == root {
+		return root
+	}
+	leftResult := lowestCommonAncestor(root.Left, p, q)
+	rightResult := lowestCommonAncestor(root.Right, p, q)
+	if leftResult != nil && rightResult != nil {
+		return root
+	}
+	if leftResult != nil {
+		return leftResult
+	}
+	return rightResult
+}
