a864740172045f524664f1cdf4e8c63194b79b85
Super262 <virusformail@gmail.com>
2021-04-02 23:18:40 +0800

    Finished Problem 236.

--- algorithm/divideandconquer/problem0236.go
+++ algorithm/divideandconquer/problem0236.go
@@ -1,38 +1,20 @@
 package divideandconquer
 
+// p and q will exist in the tree.
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 	if root == nil || q == nil || p == nil {
 		return nil
 	}
-	pathP := make([]*TreeNode, 0)
-	if !findPath0236(root, p, &pathP) {
-		return nil
+	if root == p || q == root {
+		return root
 	}
-	pathQ := make([]*TreeNode, 0)
-	if !findPath0236(root, q, &pathQ) {
-		return nil
+	leftResult := lowestCommonAncestor(root.Left, p, q)
+	rightResult := lowestCommonAncestor(root.Right, p, q)
+	if leftResult != nil && rightResult != nil {
+		return root
 	}
-	i := 1
-	for i < len(pathP) && i < len(pathQ) {
-		if pathP[i] != pathQ[i] {
-			break
-		}
-		i++
+	if leftResult != nil {
+		return leftResult
 	}
-	return pathP[i-1]
-}
-
-func findPath0236(root, target *TreeNode, path *[]*TreeNode) bool {
-	if root == nil || target == nil {
-		return false
-	}
-	*path = append(*path, root)
-	if root == target {
-		return true
-	}
-	if findPath0236(root.Left, target, path) || findPath0236(root.Right, target, path) {
-		return true
-	}
-	*path = (*path)[0 : len(*path)-1]
-	return false
+	return rightResult
 }
