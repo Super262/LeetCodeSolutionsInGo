38694cdd5c55a73b591c7d6b91dd8b654881826a
Super262 <virusformail@gmail.com>
2021-04-04 01:12:57 +0800

    Finished Problem 90.

--- algorithm/searchmethods/dfs/problem0090.go
+++ algorithm/searchmethods/dfs/problem0090.go
@@ -2,35 +2,26 @@ package dfs
 
 import "sort"
 
-func getCombinations0090(nums *[]int, startIndex int, endIndex int, isCaptured *[]bool, tempResult *[]int, result *[][]int) {
-	if startIndex < endIndex {
-		for i := startIndex; i < endIndex; i++ {
-			if i > 0 && (*nums)[i] == (*nums)[i-1] && !(*isCaptured)[i-1] {
-				continue
-			} else {
-				*tempResult = append(*tempResult, (*nums)[i])
-				(*isCaptured)[i] = true
-				*result = append(*result, append([]int{}, *tempResult...))
-				getCombinations0090(nums, i+1, endIndex, isCaptured, tempResult, result)
-				(*isCaptured)[i] = false
-				*tempResult = (*tempResult)[0 : len(*tempResult)-1]
-			}
+func helper0090(nums *[]int, startIndex int, subset *[]int, results *[][]int) {
+	subsetCopied := make([]int, len(*subset), len(*subset))
+	copy(subsetCopied, *subset)
+	*results = append(*results, subsetCopied)
+	for i := startIndex; i < len(*nums); i++ {
+		if i > startIndex && (*nums)[i-1] == (*nums)[i] {
+			continue
 		}
+		*subset = append(*subset, (*nums)[i])
+		helper0090(nums, i+1, subset, results)
+		*subset = (*subset)[0 : len(*subset)-1]
 	}
 }
 
 func subsetsWithDup(nums []int) [][]int {
-	result := make([][]int, 1, 1)
-	if nums == nil {
-		return result
-	}
-	numsLen := len(nums)
-	if numsLen == 0 {
-		return result
-	}
-	sort.Ints(nums)
-	isCaptured := make([]bool, numsLen, numsLen)
-	tempResult := make([]int, 0, numsLen)
-	getCombinations0090(&nums, 0, numsLen, &isCaptured, &tempResult, &result)
-	return result
+	numsCopied := make([]int, len(nums), len(nums))
+	copy(numsCopied, nums)
+	sort.Ints(numsCopied)
+	results := make([][]int, 0)
+	tempResult := make([]int, 0, len(numsCopied))
+	helper0090(&numsCopied, 0, &tempResult, &results)
+	return results
 }
