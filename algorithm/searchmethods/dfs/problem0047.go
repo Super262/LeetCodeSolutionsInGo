0f6707184ccf2784f47538d7cc09c1f84356a5de
Super262 <virusformail@gmail.com>
2021-04-04 14:32:34 +0800

    Finished Problem 47.

--- algorithm/searchmethods/dfs/problem0047.go
+++ algorithm/searchmethods/dfs/problem0047.go
@@ -2,35 +2,32 @@ package dfs
 
 import "sort"
 
-func getPermutations0047(nums *[]int, result *[][]int, visited *[]bool, tempPath *[]int, numsLen int) {
-	if len(*tempPath) == numsLen {
-		*result = append(*result, append([]int{}, *tempPath...))
-	} else {
-		for i := 0; i < numsLen; i++ {
-			if (*visited)[i] || (i > 0 && !(*visited)[i-1] && (*nums)[i] == (*nums)[i-1]) {
-				continue
-			}
-			(*visited)[i] = true
-			*tempPath = append(*tempPath, (*nums)[i])
-			getPermutations0047(nums, result, visited, tempPath, numsLen)
-			*tempPath = (*tempPath)[0 : len(*tempPath)-1]
-			(*visited)[i] = false
-		}
-	}
+func permuteUnique(nums []int) [][]int {
+	numsCopied := make([]int, len(nums), len(nums))
+	permutation := make([]int, 0, len(nums))
+	visited := make([]bool, len(nums), len(nums))
+	copy(numsCopied, nums)
+	results := make([][]int, 0)
+	sort.Ints(numsCopied)
+	dfs0047(&numsCopied, &permutation, &visited, &results)
+	return results
 }
 
-func permuteUnique(nums []int) [][]int {
-	result := make([][]int, 0)
-	if nums == nil {
-		return result
+func dfs0047(nums, permutation *[]int, visited *[]bool, results *[][]int) {
+	if len(*permutation) == len(*nums) {
+		permutationCopied := make([]int, len(*permutation), len(*permutation))
+		copy(permutationCopied, *permutation)
+		*results = append(*results, permutationCopied)
+		return
 	}
-	numsLen := len(nums)
-	if numsLen == 0 {
-		return result
+	for i := range *nums {
+		if (*visited)[i] || (i > 0 && (*nums)[i-1] == (*nums)[i] && !(*visited)[i-1]) {
+			continue
+		}
+		(*visited)[i] = true
+		*permutation = append(*permutation, (*nums)[i])
+		dfs0047(nums, permutation, visited, results)
+		*permutation = (*permutation)[0 : len(*permutation)-1]
+		(*visited)[i] = false
 	}
-	visited := make([]bool, numsLen, numsLen)
-	tempPath := make([]int, 0)
-	sort.Ints(nums)
-	getPermutations0047(&nums, &result, &visited, &tempPath, numsLen)
-	return result
 }
