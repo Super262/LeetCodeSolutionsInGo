812a7ce90456b6c2f6d28d3810df68b4f84b0a84
Super262 <virusformail@gmail.com>
2021-04-05 19:32:36 +0800

    Reformatted code.

--- datastructure/trie/problem0208.go
+++ datastructure/trie/problem0208.go
@@ -2,19 +2,19 @@ package trie
 
 import "strings"
 
-type Trie struct {
-	sons    [26]*Trie
+type Trie0208 struct {
+	sons    [26]*Trie0208
 	isWord  bool
 	wordVal string
 }
 
 /** Initialize your data structure here. */
-func Constructor() Trie {
-	return Trie{}
+func Constructor() Trie0208 {
+	return Trie0208{}
 }
 
 /** Inserts a word into the trie. */
-func (this *Trie) Insert(word string) {
+func (this *Trie0208) Insert(word string) {
 	charArray := []rune(word)
 	arrLen := len(charArray)
 	chOrd := 0
@@ -22,7 +22,7 @@ func (this *Trie) Insert(word string) {
 	for i := 0; i < arrLen; i++ {
 		chOrd = int(charArray[i]) - 'a'
 		if p.sons[chOrd] == nil {
-			p.sons[chOrd] = &Trie{}
+			p.sons[chOrd] = &Trie0208{}
 		}
 		p = p.sons[chOrd]
 	}
@@ -31,7 +31,7 @@ func (this *Trie) Insert(word string) {
 }
 
 /** Returns if the word is in the trie. */
-func (this *Trie) Search(word string) bool {
+func (this *Trie0208) Search(word string) bool {
 	charArray := []rune(word)
 	arrLen := len(charArray)
 	chOrd := 0
@@ -47,7 +47,7 @@ func (this *Trie) Search(word string) bool {
 }
 
 /** Returns if there is any word in the trie that starts with the given prefix. */
-func (this *Trie) StartsWith(prefix string) bool {
+func (this *Trie0208) StartsWith(prefix string) bool {
 	charArray := []rune(prefix)
 	arrLen := len(charArray)
 	chOrd := 0
@@ -63,7 +63,7 @@ func (this *Trie) StartsWith(prefix string) bool {
 }
 
 /**
- * Your Trie object will be instantiated and called as such:
+ * Your Trie0208 object will be instantiated and called as such:
  * obj := Constructor();
  * obj.Insert(word);
  * param_2 := obj.Search(word);
